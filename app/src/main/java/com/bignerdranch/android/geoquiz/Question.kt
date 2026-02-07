f81203269ad27f8bd2d540445bc5a45dcced9c29
AlexandraYuya <alyp@itu.dk>
2026-02-07 13:33:49 +0100

    init commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.annotation.StringRes
+
+// Use data keyword to clearly indicate this class holds model data
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
+
